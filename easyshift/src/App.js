7d748d0f260c727c6ddc9dbcf681b603596291c2
marciuto <formante.marco@gmail.com>
2023-07-25 21:58:10 +0200

    setting sass color variables, add normalize.scss

--- easyshift/src/App.js
+++ easyshift/src/App.js
@@ -1,9 +1,12 @@
 import {Routes,Route} from 'react-router-dom'
-
+import Login from './components/Login/Login';
+import './sass/global_styles.scss'
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path={'/'} element={<Login/>} />
+      </Routes>
     </div>
   );
 }
