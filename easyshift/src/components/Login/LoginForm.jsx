689bc56b099d73f6efc1cafda2580bee9564b909
marciuto <formante.marco@gmail.com>
2023-07-26 01:26:07 +0200

    git git status

--- easyshift/src/components/Login/LoginForm.jsx
+++ easyshift/src/components/Login/LoginForm.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const LoginForm = ({handleSubmit,username,password,setUsername,setPassword}) => {
   return (
     <div>
-       <form onSubmit={handleSubmit}>
-              <div >
-                <label htmlFor="username">Username *</label>
-                <input type="text" id='username' value={username} maxLength={25} onChange={(e)=> setUsername(e.target.value)} />
-              </div>
-              <div >
-                <label htmlFor="password">Password *</label>
-                <input type="text" id='password' value={password} maxLength={60} onChange={(e)=> setPassword(e.target.value)} />
-              </div>
-               <input type="submit" value="ENTER" />
-            </form>
+       <form className='form form__center--column' onSubmit={handleSubmit}>
+            <div className='row'>
+               <label htmlFor="username">Username <span className='required'>*</span> </label>
+               <input type="text" id='username' value={username} maxLength={25} onChange={(e)=> setUsername(e.target.value)} />
+            </div>
+            <div className='row'>
+               <label htmlFor="password">Password <span className='required'>*</span> </label>
+               <input type="text" id='password' value={password} maxLength={60} onChange={(e)=> setPassword(e.target.value)} />
+            </div>
+              <input className='cta-btn' type="submit" value="ENTER" />
+        </form>
     </div>
   )
 }
