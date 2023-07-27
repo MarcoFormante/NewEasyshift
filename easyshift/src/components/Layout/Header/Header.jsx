ae256e9f2fbc9cf7d0d275da80c68eaffbb9df8f
marciuto <formante.marco@gmail.com>
2023-07-27 23:02:18 +0200

    create easyshift Logo

--- easyshift/src/components/Layout/Header/Header.jsx
+++ easyshift/src/components/Layout/Header/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav/Nav'
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
+  const [menuToggle,setMenuToggle] = useState(false)
+  
+
   return (
     <header className='header'>
       <div className='header__container'>
         <div className='header__container__left'>
-            <div className='brand-logo'>Logo</div>
+          <div className='brand-logo btn'>
+            <Link to={"/home"} className='container__flex--center' >
+              <img src="/Brand_Logo.png" alt="Easyshift - an easy way to change your day" />
+            </Link>
+          </div>
             <Nav />
         </div>
-        <div className='menu-btn btn'>
+        <div className={`menu-btn ${menuToggle ? "menu-btn--active" : ""} btn`} onClick={()=>setMenuToggle(!menuToggle)}>
           <span className='menu-btn__line'></span>
           <span className='menu-btn__line'></span>
           <span className='menu-btn__line'></span>
