c294e2148b0766a08f8dcf6f5f038c18657bc998
marciuto <formante.marco@gmail.com>
2023-07-31 19:35:59 +0200

    create request user info elements

--- easyshift/src/components/Elements/Request/UserInfo.jsx
+++ easyshift/src/components/Elements/Request/UserInfo.jsx
@@ -5,10 +5,14 @@ import ph_simple from '../../../icons/PH_simple.svg'
 const UserInfo = ({ username, role }) => {
     
   return (
-    <div className='request-card__user-info'>
-        <span>{ username.slice(0,1).toUpperCase()}</span>
-        <span>{ username}</span>
-        <span style={role === "Duty" ?{backgroundImage:"url("+ ph_duty +")"}:{backgroundImage:"url("+ ph_simple +")"}}></span>
+      <div className='request-card__user-info'>
+        <div className='request-card__user-info__left-container'>
+          <span className='request-card__user-info__slicedName'>{username.slice(0,1).toUpperCase()}</span>
+          <span className='request-card__user-info__name'>{username}</span>
+        </div>
+        <div className='request-card__user-info__right-container'>
+            <span className='request-card__user-info__role' style={role === "Duty" ?{backgroundImage:"url("+ ph_duty +")"}:{backgroundImage:"url("+ ph_simple +")"}}></span>
+        </div>
     </div>
 
     
