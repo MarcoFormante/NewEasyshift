cb6728ed953a3ea8700734f7076841043c1ad8bf
marciuto <formante.marco@gmail.com>
2023-07-31 19:50:40 +0200

    create request shift-request elements

--- easyshift/src/components/Elements/Request/Shift_Request.jsx
+++ easyshift/src/components/Elements/Request/Shift_Request.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const ShiftRequest = () => {
+const ShiftRequest = ({shiftStart,shiftEnd,request}) => {
     
   return (
-    
-    <div className='request-card__shift-request'></div>
+    <div className='request-card__shift-request'>
+      <div className='request-card__shift-request__container'>
+        <span className='request-card__shift-request__title'>Shift</span>
+        <span><time>{shiftStart}</time>/ <time>{ shiftEnd}</time></span>
+      </div>
+      <div className='request-card__shift-request__container'>
+        <span className='request-card__shift-request__title'>Request</span>
+        <span>{request}</span>
+      </div>
+    </div>
     
   )
 }
