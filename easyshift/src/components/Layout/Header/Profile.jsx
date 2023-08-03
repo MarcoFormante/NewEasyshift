f5832d0f4036b917a0ae08926fad9e1831a42d7e
marciuto <formante.marco@gmail.com>
2023-08-03 21:21:30 +0200

    create profile Elements and style

--- easyshift/src/components/Layout/Header/Profile.jsx
+++ easyshift/src/components/Layout/Header/Profile.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import { Link } from 'react-router-dom'
 
 const Profile = () => {
-    const [profile, setProfile] = useState([])
+  const [profile, setProfile] = useState([])
+  const userInfo = useSelector((state)=> state.userInfo.value)
     
+  console.log(userInfo);
   return (
-    <div>
-      Profile
+    <div className='profile'>
+      <div className='sidebar__title'>Profile</div>
+      <div className='profile__userInfo'>
+        <div>
+          <span className='txt-bold'>Name:</span>
+          <span className='txt-bold'>Role:</span>
+          <span className='txt-bold'>Requests:</span>
+        </div>
+        <div>
+          <span>{userInfo.username}</span>
+          <span>{ userInfo.role}</span>
+          <span>{userInfo.requests}</span>
+        </div>
+      </div>
+      <Link to={"/deleteAccount"} className='link btn' >Delete Account</Link>
+      <Link to={"/modifyAccount"} className='link btn' style={{color:"greenyellow"}}>Modify Account</Link>
     </div>
   )
 }
