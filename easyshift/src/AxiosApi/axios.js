ade041fa44680f07c6b21b4f85ba293a9cc4517c
marciuto <formante.marco@gmail.com>
2023-08-03 22:50:45 +0200

    create axios baseUrl

---
+++ easyshift/src/AxiosApi/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default axios.create(
+    {
+        baseURL: process.env.REACT_APP_API_URL,
+        timeout: 8000,
+            headers: {
+            "Content-Type": "application/json"  
+            }
+        }
+        
+    )
