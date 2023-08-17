31e8d9379399a02b7c450ba121e235e2b3d673ca
marciuto <formante.marco@gmail.com>
2023-08-17 19:58:23 +0200

    add Alert to comment input

--- easyshift/src/components/Elements/Request/CommentInput.jsx
+++ easyshift/src/components/Elements/Request/CommentInput.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
+import { setAlert } from '../../../Redux/alertSlice'
+import { useDispatch } from 'react-redux'
 
 const CommentInput = ({ userInfo,requestID, handleAddComment }) => {
   const [comment, setComment] = useState("")
+  const dispatch = useDispatch()
   
-  
+
   const handleSendComment = () => {
     if (comment && comment.length <= 50) {
       handleAddComment({
         userId: userInfo.userID,
         requestId: requestID,
-        comment:comment.trim(),
+        comment:comment.trim().slice(0,50),
       })
       setComment("")
+      
     } else {
-      //handle no comment  with alert
+      if (comment.length > 50) {
+        dispatch(setAlert({type:"error",text:"The comment can have maximum 50 characters",title:"",timemout:2000}))
+      }
     }
   }
 
@@ -25,7 +31,7 @@ const CommentInput = ({ userInfo,requestID, handleAddComment }) => {
         rows="3"
         maxLength="50"
         value={comment}
-        onChange={(e) => setComment(e.target.value)}
+        onChange={(e) => setComment(e.target.value.slice(0,50))}
         placeholder='Write something'
       />
       <button className='cta-btn' onClick={handleSendComment}>Send</button>
